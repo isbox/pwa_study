7e8c10f044bfbaa473939cd123afdc2962e229fa
box <thereisbox@foxmail.com>
2018-10-11 23:51:31 +0800

    manifest

--- public/demo01.pwa.js
+++ public/demo01.pwa.js
@@ -1,8 +1,4 @@
 
-self.addEventListener('install', event => {
-    console.warn('123123123123123');
-})
-
 self.addEventListener('install', e => {
     e.waitUntil(
        caches.open('airhorner').then(cache => {
