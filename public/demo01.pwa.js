4610dbbf30786bf6e2778be1d04431f001ad6a8f
box <thereisbox@foxmail.com>
2018-10-11 23:49:52 +0800

    manifest

--- public/demo01.pwa.js
+++ public/demo01.pwa.js
@@ -3,6 +3,18 @@ self.addEventListener('install', event => {
     console.warn('123123123123123');
 })
 
+self.addEventListener('install', e => {
+    e.waitUntil(
+       caches.open('airhorner').then(cache => {
+           return cache.addAll([
+               '/',
+               '/public/demo01.pwa.js',
+               '/public/manifest.json'
+           ])
+       })
+    )
+});
+
 self.addEventListener('fetch', event => {
     
 })
